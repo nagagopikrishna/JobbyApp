cac455e16dae838f2bf02a19fe6c9c3d4b4da116
nagagopikrishna <nagagopikrishna4@gmail.com>
2024-06-23 10:57:18 +0000

    full completed login and home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import {Component} from 'react'
-import {Redirect} from 'react-router-dom'
+import {Redirect, Link} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import Header from '../Header'
 import './index.css'
@@ -13,6 +13,23 @@ class Home extends Component {
     return (
       <div className="main-container">
         <Header />
+        <div className="home-container-description">
+          <h1 className="main-heading"> Find The Job That Fits Your life </h1>
+          <p className="description">
+            Millions of people are searching for jobs, salary information,
+            company reviews. Find the job that fits your abilities and
+            potential.
+          </p>
+          <Link to="/jobs">
+            <button
+              type="button"
+              className="find-job-button"
+              onClick={this.onClickFindJob}
+            >
+              Find Jobs
+            </button>
+          </Link>
+        </div>
       </div>
     )
   }
--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,9 +1,21 @@
 import {Component} from 'react'
+import {BsSearch} from 'react-icons/bs'
+import Header from '../Header'
 import './index.css'
 
 class Jobs extends Component {
   render() {
-    return <h1> Job</h1>
+    return (
+      <div className="jobs-main-container">
+        <Header />
+        <div className="job-container">
+          <div className="search-container">
+            <input type="search" className="search-jobs" placeholder="Search" />
+            <BsSearch className="search-icon" />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
